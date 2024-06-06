bc7558a5b30b15e88cce4efa412579b367a73ed2
Diego Flores <dfmonroy07@gmai.com>
2024-06-06 17:21:49 -0600

    added clave form

--- subastas-stands-app/src/pages/agente.js
+++ subastas-stands-app/src/pages/agente.js
@@ -113,6 +113,29 @@ function Agente(){
                                 </a>
                             ))}
                             </div>
+                            <div>
+                                <form className="w-full max-w-md lg:col-span-5 lg:pt-20 flex">
+                                    <div className="flex gap-x-4">
+                                        <label htmlFor="email-address" className="sr-only">
+                                        Email address
+                                        </label>
+                                        <input
+                                        id="access-code"
+                                        name="accesscode"
+                                        type="password"
+                                        required
+                                        className="min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                                        placeholder="Clave de acceso"
+                                        />
+                                        <button
+                                        type="submit"
+                                        className="flex-none rounded-md bg-indigo-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                                        >
+                                            Enviar
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
                         </figcaption>
                     </div>
                 </div>
